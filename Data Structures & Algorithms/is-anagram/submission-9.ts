8de9e3255265d81c0bb3dc17650f93ce09834b93
Vladyslav Sak <vladyslav.sak@gmail.com>
2026-08-21 16:44:32 +0300

    Add: is-anagram - submission-9

---
+++ Data Structures & Algorithms/is-anagram/submission-9.ts
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+        if (s.length !== t.length) {
+            return false;
+        }
+
+        const seenMap = new Map<string, number>();
+
+        for (let i = 0; i < s.length; i++) {
+            seenMap.set(s[i], (seenMap.get(s[i]) ?? 0) + 1);
+        }
+
+        for (let j = 0; j < t.length; j++) {
+            const charCount = seenMap.get(t[j]);
+            if (!seenMap.has(t[j])) {
+                return false;
+            }
+
+            charCount - 1 === 0 ? seenMap.delete(t[j]) : seenMap.set(t[j], charCount - 1);
+        }
+        return seenMap.size == 0;
+    }
+}
